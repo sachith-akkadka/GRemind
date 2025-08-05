f208ce18755e9f0cd3c2cdae324ae43a22f19b06
Sachith Akkadka <sachithaakkadka@gmail.com>
2025-08-05 09:01:48 +0000

    Rename that Multi - Stop button with Start

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -590,7 +590,7 @@ export default function TasksPage() {
             {actionableTasksWithLocationCount >= 2 && (
                 <Button variant="outline" size="sm" className="h-8 gap-1" onClick={handleStartMultiStopNavigation}>
                   <Navigation className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Multi-Stop</span>
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Start</span>
                 </Button>
             )}
             <DropdownMenu>
