839269b7ffeaf35d8453af81beb047251030b86c
Sachith Akkadka <sachithaakkadka@gmail.com>
2025-08-06 09:39:56 +0000

    1/1
    Next.js 15.3.3 (stale)
    Turbopack
    Console Error
    
    Error: Failed to f

--- next.config.ts
+++ next.config.ts
@@ -9,20 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    domains: ['placehold.co', 'lh3.googleusercontent.com'],
   },
 };
 
